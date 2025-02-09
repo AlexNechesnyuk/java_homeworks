c22a0d2a17440ddcee1a16a80c43fdf315b44251
Alex Nechesnyuk <a.nechesnyuk@infinet.ru>
2025-02-09 15:29:15 +0300

    Revert "Merge pull request #1 from AlexNechesnyuk/homework_01"
    This reverts commit e56ddaa1a4f7482688e59cc7a9e63b1adc3b6291, reversing
    changes made to 5eb9bc11c0678956d4337130608c9fd5890025e2.

--- src/main/java/ru/otus/java/basic/homeworks/HelloApplication.java
+++ src/main/java/ru/otus/java/basic/homeworks/HelloApplication.java
@@ -3,7 +3,5 @@ package ru.otus.java.basic.homeworks;
 public class HelloApplication {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Hello1");
-        System.out.println("Hello2");
     }
 }
