dcfe47703d05b6ae153eab659aa6dcb853d9d199
Alex Nechesnyuk <a.nechesnyuk@infinet.ru>
2025-02-12 07:49:08 +0300

    class renamed

--- src/main/java/ru/otus/java/basic/homeworks/HelloApplication.java
+++ src/main/java/ru/otus/java/basic/homeworks/HelloApplication.java
@@ -1,6 +1,6 @@
 package ru.otus.java.basic.homeworks;
 
-public class HelloApplication {
+public class HomeWork {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
