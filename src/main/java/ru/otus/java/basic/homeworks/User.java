c977cca02a25d8d821f01767cb31e730c0eb5c65
Alex Nechesnyuk <a.nechesnyuk@infinet.ru>
2025-03-05 13:37:48 +0300

    Fix based on review

--- src/main/java/ru/otus/java/basic/homeworks/User.java
+++ src/main/java/ru/otus/java/basic/homeworks/User.java
@@ -1,7 +1,6 @@
 package ru.otus.java.basic.homeworks;
 
 
-
 public class User {
     private String name, surname, patronymic;
     private int yearOfBirth;
@@ -29,11 +28,11 @@ public class User {
         final int CURRENT_YEAR = 2025;
         User[] user = new User[10];
 
-        for (int i = 0; i<10; i++) {
-            user[i] = new User("Name" + i, "Surname" + i, "Patronymic" + i, i + 1980, "email" + i + "@.gmail.com");
+        for (int i = 0; i < user.length; i++) {
+            user[i] = new User("Name" + i, "Surname" + i, "Patronymic" + i, i + 1980, "email" + i + "@gmail.com");
         }
 
-        for (User u: user) {
+        for (User u : user) {
             if (CURRENT_YEAR - u.getYearOfBirth() > 40) {
                 u.traceInfo();
                 System.out.println();
