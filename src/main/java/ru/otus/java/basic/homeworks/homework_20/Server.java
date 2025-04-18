a75d91df22bce8577b8ce2f56d39ca52a71a02d0
Alex Nechesnyuk <a.nechesnyuk@infinet.ru>
2025-04-18 18:24:30 +0300

    Fix based on review

--- src/main/java/ru/otus/java/basic/homeworks/homework_20/Server.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework_20/Server.java
@@ -25,35 +25,9 @@ public class Server {
                 client.close();
                 continue;
             }
-            String result = calc(userInput);
+            String result = Calculator.calc(userInput);
             outputStream.writeUTF(result);
             outputStream.flush();
         }
-    }
-
-    private static String getUpperCaseUserIput(String userInput) {
-        System.out.println("начали трансформацию! ");
-        return userInput.toUpperCase();
-    }
-
-    private static String calc(String userInput) {
-        Pattern p = Pattern.compile("(-?[0-9]+) *([-*+/]) *(-?[0-9]+)");
-        Matcher m = p.matcher(userInput);
-        if(m.matches()) {
-            int A = Integer.parseInt(m.group(1));
-            String operator = m.group(2);     // ([-*+/])
-            int B = Integer.parseInt(m.group(3));
-            switch(operator) {
-                case "+":
-                    return Integer.toString(A+B);
-                case "-":
-                    return Integer.toString(A-B);
-                case "*":
-                    return Integer.toString(A*B);
-                case "/":
-                    return Float.toString((float)A/B);
-            }
-        }
-        return "Ошибка в выражении " + userInput;
     }
 }
