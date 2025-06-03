367fa502e82aed27a1fefb42d7428e36d52c1f9b
Alex Nechesnyuk <a.nechesnyuk@infinet.ru>
2025-06-03 19:29:20 +0300

    homework 25

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework_25/Fruit.java
@@ -0,0 +1,14 @@
+package ru.otus.java.basic.homeworks.homework_25;
+
+public class Fruit {
+    private int weight;
+    private String name;
+
+    public Fruit(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
